4953c014dca48696fee85800c5661c9c86d0b801
maximlivshitz <maxim.livshitz@locusview.com>
2022-06-28 15:05:22 +0300

    MONGO_DB_URI

--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -10,12 +10,15 @@ export const databaseProviders = [
     provide: DATABASE_PROVIDER,
     useFactory: (service: ConfigService<EnvironmentVariables>): Promise<typeof mongoose> => {
       const mongoUri = process.env.MONGO_DB_URI;
+
       mongoose.connection.on('connected', () => {
         console.log(chalk.greenBright('mongoose connected %s'), mongoUri);
       });
+
       mongoose.connection.on('error', (err) => {
         console.log(chalk.greenBright('mongoose error %s', err), mongoUri);
       });
+
       const options = {
         useNewUrlParser: true,
         useFindAndModify: false,
